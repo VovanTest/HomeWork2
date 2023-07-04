45d6f0b92b054ff30a3ef564527d7039aa1592c2
VladimirTikhonov <Vovantikhonov@inbox.ru>
2023-07-04 23:04:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 17500;
+        int miles = price/20;
+        System.out.println("Вам начислится " + miles + " миль.");
+
+    }
+}
