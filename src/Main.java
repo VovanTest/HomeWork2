3da3bfa1b9ea63c0d148fbe05cdfc8b0f67896bb
VladimirTikhonov <Vovantikhonov@inbox.ru>
2023-07-17 22:38:36 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int price = 17500;
-        int miles = price/20;
+        int miles = price / 20;
         System.out.println("Вам начислится " + miles + " миль.");
 
     }
